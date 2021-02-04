a2812f6ba74d9d7a151c18fcacd63feee084fa7e
bbnoyung <t.prays@mail.ru>
2021-02-04 11:19:49 +0300

    Вторая домашка. Второе задание. Исправила тип для площади.

--- work5/main.cpp
+++ work5/main.cpp
@@ -2,16 +2,17 @@
 #include <math.h>
 int main()
 {
-    int R,a,d,s1,s2,s3;
-    printf("Enter R, a and d\n");
-    scanf("%d%d%d",&R,&a,&d);
-    s1=M_PI*R*R;
-    s2=(a*a*sqrt(3))/4;
-    s3=d*d;
-    printf("Result: %d",
-           (s1>s2 && s1>s3)
+    int R,a,d;
+    double s1,s2,s3;
+    printf( "Enter R, a and d\n" );
+    scanf( "%d%d%d",&R,&a,&d );
+    s1=M_PI * R * R;
+    s2=(a * a * sqrt(3))/4;
+    s3= d * d;
+    printf("Result: %.6lf",
+           (s1 > s2 && s1 > s3)
            ? s1
-           :(s1<s2 && s2>s3)
+           :(s1 < s2 && s2 > s3)
              ? s2
              :s3);
     getchar();
