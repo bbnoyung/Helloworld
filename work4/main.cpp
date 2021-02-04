328d64789de62214129f6ab70db0865e9fd27698
bbnoyung <t.prays@mail.ru>
2021-02-04 11:34:36 +0300

    Вторая домашка. Первое задание. Исправила 0.5*log

--- work4/main.cpp
+++ work4/main.cpp
@@ -2,11 +2,12 @@
 #include <math.h>
 int main()
 {
-    double x,y;
-    printf("Enter x\n");
-    scanf("%lf",&x);
-    y=((2*cos(x-M_PI/6)+sqrt(2))/(0.5*log(x)+pow(sin(x*x),2)))*exp(3*x);
-    printf("y=2*cos(x-pi/6)+sqrt(2)/1/2*lnx+sin^2(x)^2*e^3x=%.10lf\n",y);
+    double x;
+    double y;
+    printf( "Enter x " );
+    scanf( "%lf",&x );
+    y = ((2 * cos(x - M_PI/6) + sqrt(2))/((1/(2 * log(x))) + pow(sin(x * x),2))) * exp(3 * x);
+    printf("y = 2*cos(x-pi/6)+sqrt(2)/1/2*lnx+sin^2(x)^2*e^3x = %.10lf\n", y);
     getchar();
     return 0;
 }
