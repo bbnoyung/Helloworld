a0b8852e09ea21429f96f3b5b7d680ea261a75ee
bbnoyung <t.prays@mail.ru>
2021-02-05 15:42:17 +0300

    Третья домашка. Четвертое задание

---
+++ work9/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num;
+    int N = 0;
+    int i;
+    printf ("Enter number: ");
+    scanf("%d", &num);
+    for(i=0; num>0; i++)
+        {
+            N+=(num%2)*pow(10.0,i);
+            num/=2;
+        }
+    printf ( "Result: %d", N );
+    return 0;
+}
