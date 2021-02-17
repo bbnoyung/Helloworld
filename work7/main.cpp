dae6f4222be518777d0e6282577652706a5ed496
bbnoyung <t.prays@mail.ru>
2021-02-17 21:23:09 +0300

    Третья домашка. Второе задание. Исправила замечания

--- work7/main.cpp
+++ work7/main.cpp
@@ -2,17 +2,25 @@
 
 int main()
 {
-    int N1, N2, N3;
+    int N1;
+    int N2;
+    int N3;
     int del = 1;
+
     printf( "Please enter three numbers: " );
-    scanf( "%d%d%d", &N1, &N2, &N3 );
-    for(int i = 1; i <= N1; i++)
+    scanf( "%d%d%d", &N1, &N2, &N3 );  
+
+    if ( N1 == 0 || N2 == 0 || N3 == 0 )
       {
-          if(N1 % i == 0 && N2 % i == 0 && N2 % i == 0)
+        printf ( "Common divisor is not found" );
+        return 0;
+      }
+    for(int i = 1; i < 9 ; i++)
+      {
+          if((N1 % i == 0) && (N2 % i == 0) && (N3 % i == 0))
           {
               del = i;
           }
-
       }
 
     printf( "Least common divisor is: %d",del );
