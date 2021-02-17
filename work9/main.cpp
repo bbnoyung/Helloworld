b64a5df8ac5701b9d9d22492b75c73e5e7e7320e
bbnoyung <t.prays@mail.ru>
2021-02-17 21:38:13 +0300

    Третья домашка. Четвертое задание. Исправила замечания

--- work9/main.cpp
+++ work9/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
     int num;
@@ -7,11 +8,12 @@ int main()
     int i;
     printf ("Enter number: ");
     scanf("%d", &num);
-    for(i=0; num>0; i++)
-        {
-            N+=(num%2)*pow(10.0,i);
-            num/=2;
-        }
+    for(i=0; num > 0; ++i)
+    {
+        N+=(num%2)*pow(10.0,i);
+        num/=2;
+    }
     printf ( "Result: %d", N );
+
     return 0;
 }
