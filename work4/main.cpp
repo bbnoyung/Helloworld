3516e3842fb630dca679debce1136939b2c7f337
bbnoyung <t.prays@mail.ru>
2021-02-17 16:27:05 +0300

    Вторая домашка. Первое задание. Исправила замечания.

--- work4/main.cpp
+++ work4/main.cpp
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h>
+
 int main()
 {
     double x;
     double y;
-    printf( "Enter x " );
+    printf( "Enter x: " );
     scanf( "%lf",&x );
     y = ((2 * cos(x - M_PI/6) + sqrt(2))/((1/(2 * log(x))) + pow(sin(x * x),2))) * exp(3 * x);
     printf("y = 2*cos(x-pi/6)+sqrt(2)/1/2*lnx+sin^2(x)^2*e^3x = %.10lf\n", y);
-    getchar();
+
     return 0;
 }
