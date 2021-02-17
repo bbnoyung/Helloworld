f2ebe801db9829a2c2d2c803843d98929579553c
bbnoyung <t.prays@mail.ru>
2021-02-17 16:40:53 +0300

    Вторая домашка. Второе задание. Исправила замечания.

--- work5/main.cpp
+++ work5/main.cpp
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h>
+
 int main()
 {
-    int R,a,d;
-    double s1,s2,s3;
-    printf( "Enter R, a and d\n" );
+    int R;
+    int a;
+    int d;
+
+    printf( "Enter R, a and d: " );
     scanf( "%d%d%d",&R,&a,&d );
-    s1=M_PI * R * R;
-    s2=(a * a * sqrt(3))/4;
-    s3= d * d;
-    printf("Result: %.6lf",
-           (s1 > s2 && s1 > s3)
-           ? s1
-           :(s1 < s2 && s2 > s3)
-             ? s2
-             :s3);
-    getchar();
+    double s_circle = M_PI * R * R;
+    double s_triangle =(a * a * sqrt(3.))/4;
+    double s_square = d * d;
+    printf( "Result: %.5lf",
+           ( s_circle > s_triangle && s_circle > s_square)
+           ? s_circle
+           :(s_circle < s_triangle && s_triangle > s_square)
+             ? s_triangle
+             : s_square);
+
     return 0;
 }
