ea5016507550fbaa5a99c51853d9e8f65061c294
bbnoyung <t.prays@mail.ru>
2021-02-18 14:48:14 +0300

    Третья домашка. Третье задание. Исправила замечания

--- work8/main.cpp
+++ work8/main.cpp
@@ -1,22 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int num, num1;
-    int num2, num3;
+    int number;
+    int middle;
+    int last_number;
     printf("Enter number: ");
-    scanf("%d", &num);
-    num3 = num % 10;
-    num /= 10;
-    num2 = 10;
-    num1 = 0;
-    while (num > 9)
+    scanf("%d", &number);
+
+    last_number = number%10;
+    int first_number = number/10;
+    int tens = 10;
+    middle = 0;
+    while (first_number > 9)
     {
-        num1 += (num % 10) * num2;
-        num2 *= 10;
-        num /= 10;
+        middle += (first_number%10) * tens;
+        tens *= 10;
+        first_number /= 10;
     }
-    num1 = num2 * num3 + num1 + num;
-    printf("Changed number: %d\n", num1);
+    int new_number = last_number * tens + middle + first_number;
+    printf("Changed number: %d\n", new_number);
+
     return 0;
 }
