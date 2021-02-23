28a3459908fde56edc8f92e9ca77b531432209d3
bbnoyung <t.prays@mail.ru>
2021-02-23 23:05:05 +0300

    Третья домашка. Первое задание. Перенесла в функцию.

--- work6/main.cpp
+++ work6/main.cpp
@@ -7,13 +7,7 @@ int main()
     unsigned long int N;
     printf( "Please, enter N: " );
     scanf( "%lu", &N );
-//    int k = 0;
-//    do
-//    {
-//        N /= 10;
-//        k = k + 1;
-//    }
-//    while( Digits( N ) );
+
     int k = Digits( N );
     printf( "Number of digits is: %d",k );
 
